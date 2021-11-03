6cfa4aabe99e947de0279bd6b19332330e81189d
Dalton <Daltonjavery@gmail.com>
2021-11-03 18:27:49 -0500

    hopefully final push for displays, no promises

--- GoalDisplay.py
+++ GoalDisplay.py
@@ -1,53 +1,57 @@
 from Node import Node
 
-root = Node(value="rrrbggg",cost=0,level=0)
+root = Node(value="rrrbggg", cost=0, level=0)
 
 tree = {}
 valueTree = {}
 
-levelCount = {0:1}
+levelCount = {0: 1}
 needToAdd = []
 
+
 def createTree(node):
-	tree[node] = node.getChildren()
-	count=0
-	for node in tree[node]:
-		count+=1
-		needToAdd.append(node)
-	if(node.level in levelCount):
-		levelCount[node.level] += count
-	else:
-		levelCount[node.level] =count
+    tree[node] = node.getChildren()
+    count = 0
+    for node in tree[node]:
+        count += 1
+        needToAdd.append(node)
+    if node.level in levelCount:
+        levelCount[node.level] += count
+    else:
+        levelCount[node.level] = count
+
 
 createTree(root)
 
-while (len(needToAdd)>0):
-	createTree(needToAdd.pop(0))
+while len(needToAdd) > 0:
+    createTree(needToAdd.pop(0))
 
 
-def pathToGoal(node,arr):
-	
-	if(node.parent!=None):
-		arr = pathToGoal(node.parent,arr)
-	arr += " -> "+node.value
-	return arr
+def pathToGoal(node, arr):
+    if node.parent is not None:
+        arr = pathToGoal(node.parent, arr)
+    arr += " -> " + node.value
+    return arr
+
 
 # Start BFS
 visited = []
 queue = []
 
-def bfs(visited, tree, node):
-	visited.append(node)
-	queue.append(node)
 
-	while(queue):
-		curr = queue.pop(0)
-		if (curr.isGoal()):
-			print("\nLevel Found: "+str(curr.level)+" Total Cost: "+str(curr.totalCost)+" State: "+curr.value)
-			print("Path from root: "+ str(pathToGoal(curr,"")),end="\n\n")
-		for neighbor in tree[curr]:
-			if (neighbor not in visited):
-				visited.append(neighbor)
-				queue.append(neighbor)
+def bfs(visited, tree, node):
+    visited.append(node)
+    queue.append(node)
+
+    while queue:
+        curr = queue.pop(0)
+        if curr.isGoal():
+            print("\nLevel Found: " + str(curr.level) + " Total Cost: " + str(curr.totalCost) + " State: " + curr.value)
+            print("Path from root: " + str(pathToGoal(curr, "")), end="\n\n")
+        for neighbor in tree[curr]:
+            if neighbor not in visited:
+                visited.append(neighbor)
+                queue.append(neighbor)
+
 
 bfs(visited, tree, root)
--- TreeDisplay.py
+++ TreeDisplay.py
@@ -1,54 +1,52 @@
 from Node import Node
-import csv
-root = Node(value="rrrbggg",cost=0,level=0)
 
-tree = {}
+root = Node(value="rrrbggg", cost=0, level=0)
 
-levelValues = {}
-
-levelCount = {0:1}
-needToAdd = []
-seen = []
+tree = {}  # for node traversal
+levelValues = {}  # for tree display purposes, needed values so legible to human readers
+levelCount = {0: 1}  # holds the number of nodes at each level
+needToAdd = []  # used as queue to determine next node to evaluate
 
 
 def createTree(node):
-		tree[node] = node.getChildren()
-		count=0
-		for node in tree[node]:
-			count+=1
-			print(str(node.level)+" "+node.value)
-			needToAdd.append(node)
-		if(node.level in levelCount):
-			levelCount[node.level] += count
-		else:
-			levelCount[node.level] =count
-
-createTree(root)
-
-while (len(needToAdd)>0):
-	createTree(needToAdd.pop(0))
+    if not (node in tree):
+        tree[node] = node.getChildren()
+    count = 0
+    for node in tree[node]:
+        count += 1
+        # print(str(node.level)+" "+node.value)
+        needToAdd.append(node)
+    if node.level in levelCount:
+        levelCount[node.level] += count
+    else:
+        levelCount[node.level] = count
 
 
+needToAdd.append(root)
+
+while len(needToAdd) > 0:
+    createTree(needToAdd.pop(0))
 
 for x in tree:
-	if ((x.level in levelValues)):
-		levelValues[x.level].append(x.value)
-	else:
-		levelValues[x.level] = [x.value]
+    if x.level in levelValues:
+        levelValues[x.level].append(x.value)
+    else:
+        levelValues[x.level] = [x.value]
 
 # open the file in the write mode
-#f = open('test.csv', 'w')
+# f = open('test.csv', 'w')
 # create the csv writer
-#writer = csv.writer(f)
+# writer = csv.writer(f)
 
 # close the file
 
 
-
 print(levelCount)
+i = 0
 for x in levelValues:
-	print(str(len(levelValues[x]))+" "+str(levelValues[x]))
-	# write a row to the csv file
-	#writer.writerow(levelValues[x])
+    print("Level " + str(i) + ": " + str(levelValues[x]))
+    i += 1
+# write a row to the csv file
+# writer.writerow(levelValues[x])
 
-#f.close()
+# f.close()
